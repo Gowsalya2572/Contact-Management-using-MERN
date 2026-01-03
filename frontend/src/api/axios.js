7d26fa9c3b9c7297e8f26a9f3305705bdb691fb3
Gowsalya2572 <gowsalya2572@gmail.com>
2026-01-03 18:19:46 +0530

    updated

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://contact-management-backend-ten.vercel.app";
+const API_URL = "https://contact-management-backend-ten.vercel.app/api";
 
 const api = axios.create({
   baseURL: API_URL,
