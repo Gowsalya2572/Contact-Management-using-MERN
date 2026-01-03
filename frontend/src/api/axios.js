891d5f80f5b6893031b04aed7dd8457174131b86
Gowsalya2572 <87688391+Gowsalya2572@users.noreply.github.com>
2026-01-03 17:52:43 +0530

    backend url added

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:7000/api";
+const API_URL = "https://contact-management-backend-ten.vercel.app";
 
 const api = axios.create({
   baseURL: API_URL,
